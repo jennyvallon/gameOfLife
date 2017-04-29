d182ec3c5c6f86c12ca27f77b8c5c8b6c47286e5
Jenny Vallon <jennyvallon@gmail.com>
2017-04-29 18:26:10 -0400

    ammend test organization. added 'Board' and 'Cell' tests;

--- spec/GameOfLife.spec.js
+++ spec/GameOfLife.spec.js
@@ -1,12 +1,17 @@
 //test
 describe("Game of Life", function() { 
-    it("should have a board",function(){
-        expect(board).toBeTruthy();
+    describe("Board", function() { 
+        it("should have a board",function(){
+            expect(board).toBeTruthy();
+        });
+        it("board should be created dynamically",function(){
+            var x=6; 
+            var y=8;
+            expect(board.create(x,y).length).toEqual(x);
+            expect(board.create(x,y)[0].length).toEqual(y);
+        });
     });
-    it("board should be created dynamically",function(){
-        var x=6; 
-        var y=8;
-        expect(board.create(x,y).length).toEqual(x);
-        expect(board.create(x,y)[0].length).toEqual(y);
+    describe("Cell", function(){
+        
     });
 });
